93e5d9fcc45b176f9c635661beae2d8520a579b7
aayusht200 <aayush.t200@gmail.com>
2025-06-12 19:44:43 -0400

    added grid structure using css

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const container = document.querySelector(".container");
 // console.log(container);
 
-for (let i = 0; i < 15; i++) {
+for (let i = 1; i < 17; i++) {
   createDiv("div_" + i);
 }
 function createDiv(name) {
   const div_name = document.createElement("div");
-  container.appendChild(div_name);
   div_name.classList.add("grid_div");
+  div_name.textContent = "place holder";
+  container.appendChild(div_name);
+  console.log(name);
 }
