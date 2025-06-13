ff28e82e464f3a41a8369c6b30b07ebba3b4e34e
aayusht200 <aayush.t200@gmail.com>
2025-06-13 01:18:13 -0400

    done

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const container = document.querySelector(".container");
 // console.log(container);
 
-for (let i = 1; i < 17; i++) {
-  createDiv("div_" + i);
-}
-function createDiv(name) {
+window.addEventListener("load", () => {
+  //   console.log("119");
+  for (let i = 1; i < 17; i++) {
+    createDiv(4);
+  }
+});
+
+function createDiv(input) {
   const div_name = document.createElement("div");
-  div_name.classList.add("grid_div");
-//   div_name.textContent = "place holder";
+  div_name.classList.add("grid_div_default");
+  div_name.style.flex = `1 0 calc((100%)/${input})`;
+  div_name.addEventListener("mouseenter", () => {
+    div_name.style.backgroundColor = `rgb( ${randomNumber()},${randomNumber()},${randomNumber()})`;
+  });
+  div_name.addEventListener("mouseleave", () => {
+    div_name.style.backgroundColor = "white";
+  });
   container.appendChild(div_name);
-  console.log(name);
+}
+
+const btn = document.querySelector("button");
+// console.log(btn);
+
+btn.addEventListener("click", () => {
+  const user_input = parseInt(prompt("Enter a number between 1-100"));
+  //   console.log(user_input);
+  createGrid(user_input);
+});
+
+function createGrid(user_input) {
+  container.replaceChildren();
+  for (let i = 0; i < user_input * user_input; i++) {
+    createDiv(user_input);
+  }
+}
+
+function randomNumber() {
+  return Math.floor(Math.random() * 255);
 }
